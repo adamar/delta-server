7be0a36b09c4f3e2b4ce86ef2a4816c94b17c24a
Andrew Martin <a.a.d.martin@gmail.com>
2016-11-15 17:48:52 +1100

    added migration script

---
+++ models/migrations.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/adamar/delta-server/delta"
+	"github.com/adamar/delta-server/models"
+)
+
+func main() {
+
+	DBi := delta.SetupGormDB()
+	if err != nil {
+		panic(err)
+	}
+
+	DBi.AutoMigrate(&models.Event{})
+
+}
